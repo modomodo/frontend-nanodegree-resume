abecb34b55618ede59715f089720ad238e7ba687
Michael Muita <mugz.mike@gmail.com>
2016-02-12 16:07:49 +0300

    Working Implementation of Structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,18 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     pngquant = require('imagemin-pngquant'),
     responsive = require('gulp-responsive'),
-    runSequence = require('run-sequence');
+    runSequence = require('run-sequence'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    useref = require('gulp-useref');
 
 //Content Task - Copies to dist and refresh browserSync with correct img sources
+//Use reference Task - Take HTML build blocks and replace their references
 gulp.task('content', function(){
   gulp.src('./src/index.html')
-    .pipe(gulp.dest('./dist/'))
-    .pipe(browserSync.reload({stream: true}))
+      .pipe(useref())
+      .pipe(gulp.dest('./dist/'))
+      .pipe(browserSync.reload({stream: true}))
 });
 
 //Styles Task - Minifies CSS, Copies to dist and refresh browserSync
@@ -24,18 +29,6 @@ gulp.task('styles', function(){
 //Image Task - Compress and create responsive img
 gulp.task('image', function(){
   gulp.src('./src/img/*')
-    .pipe(responsive({
-      '*': [{
-        width: 800,
-        quality: 80,
-        progressive: true
-      },{
-        width: 800 * 2,
-        rename: {
-          suffix: '@2x'
-        }
-      }]
-    }))
     .pipe(imagemin({
       optimisationLevel: 3,
       interlaced: true,
@@ -43,9 +36,19 @@ gulp.task('image', function(){
       svgoPlugins: [{removeViewBox: false}],
       use: [pngquant()]
     }))
+    .pipe(gulp.dest('./dist/img'))
 });
 
-//Watch task - Watches CSS or HTML changes
+//JavaScript Task - Minify JavaScript files
+gulp.task('minjs', function(){
+  gulp.src('./src/js/*')
+      .pipe(concat('combined.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('./dist/js'))
+      .pipe(browserSync.reload({stream: true}))
+});
+
+//Watch task - Watches CSS, JS or HTML changes
 gulp.task('watch', function(){
   browserSync.init({
     server: {
@@ -55,12 +58,26 @@ gulp.task('watch', function(){
 
   gulp.watch('./src/index.html', ['content']); //Array is a task to be executed
   gulp.watch('./src/css/*.css', ['styles']);
+  gulp.watch('./src/js/*.js', ['minjs']);
 });
 
 //Use runSequence plugin to run image task first, and others after in a concurrent manner
 gulp.task('default', function (){
   runSequence(
     'image',
-    ['styles','content','watch']
+    ['styles','content','minjs','watch']
   )
 });
+
+// .pipe(responsive({
+//   '*': [{
+//     width: 800,
+//     quality: 80,
+//     progressive: true
+//   },{
+//     width: 800 * 2,
+//     rename: {
+//       suffix: '@2x'
+//     }
+//   }]
+// }))
--- src/js/resumeBuilder.js
+++ src/js/resumeBuilder.js
@@ -1,5 +1,71 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//----------------------------------------
+//  UTILITIES & GENERAL FUNCTIONS
+//----------------------------------------
 
-$("#main").append("Michael");
+var amountScrolled = 200;
+
+$(document).ready(function(){
+  //---------------------------------------------
+  // Smooth scroll function for Navigation links
+  //---------------------------------------------
+  $(".mdl-navigation__link").on('click', function(event) {
+    event.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $("html, body").stop().animate({
+      scrollTop: $target.offset().top
+    }, 1000, function () {
+      window.location.hash = target;
+    });
+  });
+
+  //---------------------------------------------
+  // FLOATING ACTION BUTTON BEHAVIOUR
+  //---------------------------------------------
+  $(window).scroll(function() {
+    if ( $(window).scrollTop() > amountScrolled ) {
+      $('a.to-top').fadeIn('slow');
+    } else {
+      $('a.to-top').fadeOut('slow');
+    }
+  });
+
+  $('a.to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 700);
+    return false;
+  });
+});
+
+
+
+//----------------------------------
+//  First practice of JS Append
+//
+//  $("#main").append("Michael");
+//---------------------------------
+
+
+//Practice of Javascript replace method
+var email = "michael.muita.dev@udacity.com";
+var newEmail = email.replace("udacity", "gmail");
+
+var formattedName = HTMLheaderName.replace("%data%","MICHAEL MUITA");
+var formattedRole = HTMLheaderRole.replace("%data%","FULL STACK ENGINEER");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+/*
+console.log(email);
+console.log(newEmail);
+
+var awesomeThoughts = "My name is Michael and I'm AWESOME!";
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);
+*/
